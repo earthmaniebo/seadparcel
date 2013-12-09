4ddd454e058b5a10b97255f06f886b924bbe85cc
Earth Ponce Maniebo <earthmaniebo@gmail.com>
2013-12-09 13:02:38 +0800

    Add address in the top part of pdf and alignment of printed by

--- admin/download-billing.php
+++ admin/download-billing.php
@@ -14,7 +14,10 @@
         }
         
         $grandTotal = $total + $vat;
-        $header = "<style>td{padding: 0 20px 0 20px; text-align:center;} th{text-align:center;}</style><h1 text-align='center'>Billing Reports of Sead Parcel Services</h1><p text-align='center'><strong>Client: </strong>".$client."<br><strong>From:</strong> ". $_POST["fromDate"] . " <strong>To:</strong> ". $_POST["toDate"] ."</p><br><br>";
+        $header = "<style>td{padding: 0 20px 0 20px; text-align:center;} th{text-align:center;}</style><p><strong>Sead Parcel Services</strong><br>4113 Kalayaan Avenue<br>
+Brgy Olympia<br>
+Makati City<br>
+</p><h1 text-align='center'>Billing Reports of Sead Parcel Services</h1><p text-align='center'><strong>Client: </strong>".$client."<br><strong>From:</strong> ". $_POST["fromDate"] . " <strong>To:</strong> ". $_POST["toDate"] ."</p><br><br>";
 
         $table = "<table style='margin:50px 0 0 780px;'>";
         if(isset($_POST["vat"])) {
@@ -41,7 +44,7 @@
                     </table>";
 
         $footer = $table . $displayTotal . $displayVat . $displayGrandTotal;
-        $footer2 = "<br><p text-align='center'><strong>Printed by:</strong> " . $_SESSION["firstName"] . " " . $_SESSION["lastName"] . "</p>";
+        $footer2 = "<br><p style='margin: 0 0 0 803px'><strong>Printed by:</strong> " . $_SESSION["firstName"] . " " . $_SESSION["lastName"] . "</p>";
         $content = $header . $_POST["content"] . $footer . $footer2;
         require_once('../html2pdf/html2pdf.class.php');
         $html2pdf = new HTML2PDF('L','A4','fr');
--- data-analyst/download-shipment.php
+++ data-analyst/download-shipment.php
@@ -6,9 +6,12 @@
         if($_POST["client"] != 'DEFAUL') {
             $client = $_POST["client"];
         }
-        $header = "<style>td{padding: 0 20px 0 20px; text-align:center;} th{text-align:center;}</style><h1 text-align='center'>Shipment Reports of Sead Parcel Services</h1><p text-align='center'><strong>Client: </strong>".$client."<br><strong>From:</strong> ". $_POST["fromDate"] . " <strong>To:</strong> ". $_POST["toDate"] . "</p><br><br>";
+        $header = "<style>td{padding: 0 20px 0 20px; text-align:center;} th{text-align:center;}</style><p><strong>Sead Parcel Services</strong><br>4113 Kalayaan Avenue<br>
+Brgy Olympia<br>
+Makati City<br>
+</p><h1 text-align='center'>Shipment Reports of Sead Parcel Services</h1><p text-align='center'><strong>Client: </strong>".$client."<br><strong>From:</strong> ". $_POST["fromDate"] . " <strong>To:</strong> ". $_POST["toDate"] . "</p><br><br>";
         
-        $footer = "<br><p text-align='center'><strong>Printed by:</strong> " . $_SESSION["firstName"] . " " . $_SESSION["lastName"] . "</p>";
+        $footer = "<br><p style='margin: 0 0 0 1262px'><strong>Printed by:</strong> " . $_SESSION["firstName"] . " " . $_SESSION["lastName"] . "</p>";
         $content = $header . $_POST["content"] . $footer;
         require_once('../html2pdf/html2pdf.class.php');
         $html2pdf = new HTML2PDF('L','A3','fr');
