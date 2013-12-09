5a553de5927658d8b961571905c0d3c69b6b3838
Earth Ponce Maniebo <earthmaniebo@gmail.com>
2013-12-09 16:32:00 +0800

    With errors

--- data-analyst/print-airwaybill.php
+++ data-analyst/print-airwaybill.php
@@ -3,7 +3,7 @@
     include("airwaybill.php");
     $content = ob_get_clean();
     require_once('../html2pdf/html2pdf.class.php');
-    $html2pdf = new HTML2PDF('L', 'A4','en', false, 'ISO-8859-15', array(5,5,0,0));
+    $html2pdf = new HTML2PDF('L', 'A4','en');
     $html2pdf->writeHTML($content, isset($_GET['vuehtml']));
     $html2pdf->Output('exemple.pdf');
 
